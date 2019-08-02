da22aed10dac2ceaf373cd329bb5a04c82d1c917
Eric Schaefer <eric@eyeem.com>
2019-08-02 15:33:59 +0200

    Flow fix

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
 // @flow
-import type { FieldNode } from 'graphql';
-import { GraphQLError } from 'graphql';
+import type { FieldNode } from "graphql";
+import { GraphQLError } from "graphql";
 
 type Context = {
-	reportError: Function
+  reportError: Function
 };
 
-const IntrospectionWhiteList = (whitelist: string[]) => (context: Context) => ({
-	Field(node: FieldNode) {
-		if (process.env.NODE_ENV === 'production') {
-			if (node.name.value === '__schema' || node.name.value === '__type') {
-				const isWhiteListed = whitelist.some(
-					item =>
-						node.arguments &&
-						node.arguments.some(arg => arg.value.value === item)
-				);
+const IntrospectionWhiteList = (
+  whitelist: string[],
+  isEnabled: boolean = true
+) => (context: Context) => ({
+  Field(node: FieldNode) {
+    if (isEnabled) {
+      if (node.name.value === "__schema" || node.name.value === "__type") {
+        const isWhiteListed = whitelist.some(
+          item =>
+            node.arguments &&
+            node.arguments.some(
+              ({ value }) => value && value.value && value.value === item
+            )
+        );
 
-				if (!isWhiteListed) {
-					context.reportError(
-						new GraphQLError(
-							'GraphQL introspection is not allowed, but the query contained __schema or __type',
-							[node]
-						)
-					);
-				}
-			}
-		}
-	}
+        if (!isWhiteListed) {
+          context.reportError(
+            new GraphQLError(
+              "GraphQL introspection is not allowed, but the query contained __schema or __type",
+              [node]
+            )
+          );
+        }
+      }
+    }
+  }
 });
 
 export default IntrospectionWhiteList;
