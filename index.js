2cdcbd87e981acaa48dd6d46c37332599583577b
Eric Schaefer <eric@eyeem.com>
2019-08-02 16:58:42 +0200

    Small refactor

--- index.js
+++ index.js
@@ -6,20 +6,25 @@ type Context = {
   reportError: Function
 };
 
+type Whitelist = string[];
+
+const checkWhitelist = (whitelist: Whitelist, node: FieldNode) =>
+  whitelist.some(
+    item =>
+      node.arguments &&
+      node.arguments.some(
+        ({ value }) => value && value.value && value.value === item
+      )
+  );
+
 const IntrospectionWhiteList = (
-  whitelist: string[],
+  whitelist: Whitelist,
   isEnabled: boolean = true
 ) => (context: Context) => ({
   Field(node: FieldNode) {
     if (isEnabled) {
       if (node.name.value === "__schema" || node.name.value === "__type") {
-        const isWhiteListed = whitelist.some(
-          item =>
-            node.arguments &&
-            node.arguments.some(
-              ({ value }) => value && value.value && value.value === item
-            )
-        );
+        const isWhiteListed = checkWhitelist(whitelist, node);
 
         if (!isWhiteListed) {
           context.reportError(
